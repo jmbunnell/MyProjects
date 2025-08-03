4b735f01ed1f400f8196f7d1681dbe89a2b37a58
Jake Bunnell <jacobbunnellca@gmail.com>
2025-08-02 20:20:52 -0400

    final solution

--- MultiplyBy2new/MultiplyBy2new/main.cpp
+++ MultiplyBy2new/MultiplyBy2new/main.cpp
@@ -5,13 +5,15 @@
 
 #include <iostream>
 
-int main() {2
-    double num;
-    std::cout << "Enter an integer: ";
-    std::cin >> num;
+int main() {
     
-    double dub = num * 2;
-    std::cout << "Double that number is: " << dub << std::endl;
+    std::cout << "Enter an integer: ";
+    
+    double num{};
+    std::cin >> num;
+   
+    std::cout << "Double that number is: " << num * 2 << std::endl;
+    std::cout << "Triple that number is: " << num * 3 << std::endl;
     
     return 0;
 }
