8ba9868915fa85e5b4cd3fd386faa696339133b7
Jake Bunnell <jacobbunnellca@gmail.com>
2025-08-02 20:08:43 -0400

    Final?

--- MultiplyBy2new/MultiplyBy2new/main.cpp
+++ MultiplyBy2new/MultiplyBy2new/main.cpp
@@ -1,14 +1,17 @@
-//
-//  main.cpp
-//  MultiplyBy2new
-//
-//  Created by Jacob Bunnell on 8/2/25.
-//
+//Program asks the user to enter an integer, waits for them to input an integer, then tells them what 2 times that number is.
+
+//Enter an integer: 4
+//Double that number is: 8
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {2
+    double num;
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+    
+    double dub = num * 2;
+    std::cout << "Double that number is: " << dub << std::endl;
+    
     return 0;
 }
