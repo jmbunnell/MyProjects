aa157a9ee08214ec180214fc573a07c2da744289
Jake Bunnell <jacobbunnellca@gmail.com>
2025-08-10 18:52:44 -0400

    color to slider

--- JUCE PROJECTS /PLUG-INS/Delay/Source/LookAndFeel.cpp
+++ JUCE PROJECTS /PLUG-INS/Delay/Source/LookAndFeel.cpp
@@ -50,10 +50,25 @@ void RotaryKnobLookAndFeel::drawRotarySlider (
     g.setColour(Colors::Knob::trackBackground);
     g.strokePath(backgroundArc, strokeType);
     
-    auto dialRadius = innerRect.getHeight() / 2.0f;
+    auto dialRadius = innerRect.getHeight() / 2.0f - lineWidth;
     auto toAngle = rotaryStartAngle + sliderPos * (rotaryEndAngle - rotaryStartAngle);
     
-    juce::Point<float> dialStart(center.x, center.y);
+    juce::Point<float> dialStart(center.x + 10.0f * std::sin(toAngle),
+                                 center.y - 10.0f * std::cos(toAngle));
+    
     juce::Point<float> dialEnd(center.x + dialRadius * std::sin(toAngle),
                                center.y - dialRadius * std::cos(toAngle));
+    
+    juce::Path dialPath;
+    dialPath.startNewSubPath(dialStart);
+    dialPath.lineTo(dialEnd);
+    g.setColour(Colors::Knob::dial);
+    g.strokePath(dialPath, strokeType);
+    
+    if (slider.isEnabled()) {
+        juce::Path valueArc;
+        valueArc.addCentredArc(center.x, center.y, arcRadius, arcRadius, 0.0f, rotaryStartAngle, toAngle, true);
+        g.setColour(Colors::Knob::trackActive);
+        g.strokePath(valueArc, strokeType);
+    }
 }
